c8b0a67c84134a2bc7843c081279525357f9cbd0
onesine <onesine@dmdconsul.com>
2022-11-14 08:22:16 +0100

    Update git cache

--- dist/components/AccordionBody.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-interface Props {
-    children: JSX.Element | JSX.Element[];
-    as?: string;
-    className?: string;
-}
-declare const AccordionBody: React.FC<Props>;
-export default AccordionBody;
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import Accordion from "./components/Accordion";
-import AccordionItem from "./components/AccordionItem";
-import AccordionHeader from "./components/AccordionHeader";
-import AccordionBody from "./components/AccordionBody";
-export { Accordion, AccordionItem, AccordionHeader, AccordionBody };
